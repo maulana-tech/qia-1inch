1a4c276cc92f5919bc67b9baacd2d9d36d6ceb25
Finetune <finetuneid@gmail.com>
2026-09-10 15:40:26 +0700

    refactor(demosharedcapital.s): clear every fixable build warning

--- contracts/script/DemoSharedCapital.s.sol
+++ contracts/script/DemoSharedCapital.s.sol
@@ -144,7 +144,7 @@ contract DemoSharedCapital is Script, IqiaOpcodes {
         console.log(string.concat("VITE_DESK_MAKER=", vm.toString(maker)));
     }
 
-    function _program(uint64 saltValue) internal view returns (bytes memory) {
+    function _program(uint64 saltValue) internal pure returns (bytes memory) {
         Program memory p = ProgramBuilder.init(_opcodes());
         return bytes.concat(
             p.build(SolvencyGuard._solvencyGuardXD, SolvencyGuardArgsBuilder.build(SURCHARGE_BPS)),
